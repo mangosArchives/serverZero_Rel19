20f3babf7278eb389b870ad1bb706ea91ffb3662
Salja <salja2012@hotmail.de>
2013-01-15 23:48:18 +0100

    Fix Rage Gain of Warriors for Classic 1.12.1.
    Signed-off-by: Salja <salja2012@hotmail.de>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1797,7 +1797,7 @@ void Player::RewardRage(uint32 damage, uint32 weaponSpeedHitFactor, bool attacke
 
     if (attacker)
     {
-        addRage = ((damage / rageconversion * 7.5f + weaponSpeedHitFactor) / 2.0f);
+        addRage = 2 * damage / getLevel();
     }
     else
     {
