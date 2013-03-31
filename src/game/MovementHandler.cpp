2b4153b3ce0d5f2cdb8b461c6f2bfa571cc324ca
unknown <Stephane@.(none)>
2013-03-31 03:42:54 +0200

    [z2338] Fall under map ressurect
    Signed-off-by: Salja <salja2012@hotmail.de>

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -583,6 +583,9 @@ void WorldSession::HandleMoverRelocation(MovementInfo& movementInfo)
 
                 // cancel the death timer here if started
                 plMover->RepopAtGraveyard();
+
+                plMover->ResurrectPlayer(0.5f);
+                plMover->SpawnCorpseBones();
             }
         }
     }
