72a6c049fa9f4667c8f0155bbd1e68301bfd54ec
jaxx56 <natew1994@gmail.com>
2013-04-03 19:04:47 -0500

    WARDEN\DB Implement basic DLL signature's

--- sql/realmd.sql
+++ sql/realmd.sql
@@ -219,6 +219,11 @@ CREATE TABLE `warden_data_result` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+INSERT INTO `warden_data_result` (`id`, `check`, `data`, `str`, `address`, `length`, `result`, `comment`) VALUES 
+('1', '217', '', 'SPEEDHACK-X86_64.DLL', '', '', '', 'SPEED HACK - Injected DLL'),
+('2', '217', '', 'WPESPY.DLL', '', '', '', 'WPE PRO - Injected DLL'),
+('3', '217', '', 'RPE.DLL', '', '', '', 'rEdoX Packet Editor - Injected DLL');
+
 --
 -- Dumping data for table `warden_data_result`
 --
---
+++ sql/updates/warden_data_result.sql
@@ -0,0 +1,4 @@
+INSERT INTO `warden_data_result` (`id`, `check`, `data`, `str`, `address`, `length`, `result`, `comment`) VALUES 
+('1', '217', '', 'SPEEDHACK-X86_64.DLL', '', '', '', 'SPEED HACK - Injected DLL'),
+('2', '217', '', 'WPESPY.DLL', '', '', '', 'WPE PRO - Injected DLL');
+('3', '217', '', 'RPE.DLL', '', '', '', 'rEdoX Packet Editor - Injected DLL');
