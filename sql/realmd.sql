9e309fb017817e119e10cbfcc505ee4a8d4ff498
jaxx56 <natew1994@gmail.com>
2013-04-04 16:07:43 -0500

    Warden\DB More DLL Signatures!

--- sql/realmd.sql
+++ sql/realmd.sql
@@ -222,7 +222,11 @@ CREATE TABLE `warden_data_result` (
 INSERT INTO `warden_data_result` (`id`, `check`, `data`, `str`, `address`, `length`, `result`, `comment`) VALUES 
 ('1', '217', '', 'SPEEDHACK-X86_64.DLL', '', '', '', 'SPEED HACK - Injected DLL'),
 ('2', '217', '', 'WPESPY.DLL', '', '', '', 'WPE PRO - Injected DLL'),
-('3', '217', '', 'RPE.DLL', '', '', '', 'rEdoX Packet Editor - Injected DLL');
+('3', '217', '', 'IHOOK.DLL', '', '', '', 'ASM - Injected DLL'),
+('4', '217', '', 'WHITEMAGIC.DLL', '', '', '', 'Application Inject Library - Injected DLL'),
+('5', '217', '', 'GREYMAGIC.DLL', '', '', '', 'Automation Botting Library - Injected DLL'),
+('6', '217', '', 'BLACKMAGIC.DLL', '', '', '', 'Memory Inject Library - Injected DLL'),
+('7', '217', '', 'RPE.DLL', '', '', '', 'rEdoX Packet Editor - Injected DLL');
 
 --
 -- Dumping data for table `warden_data_result`
