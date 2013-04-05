abed132e5e4a9ada272d37f6fcc62dbca25e5023
Anton <lfxgroove@zoho.com>
2013-04-05 23:39:33 +0200

    Should fix #2 in mangos-zero

--- src/game/PointMovementGenerator.h
+++ src/game/PointMovementGenerator.h
@@ -41,7 +41,7 @@ class MANGOS_DLL_SPEC PointMovementGenerator
         MovementGeneratorType GetMovementGeneratorType() const override { return POINT_MOTION_TYPE; }
 
         bool GetDestination(float& x, float& y, float& z) const { x = i_x; y = i_y; z = i_z; return true; }
-    private:
+    protected:
         uint32 id;
         float i_x, i_y, i_z;
         bool m_generatePath;
@@ -55,6 +55,7 @@ class MANGOS_DLL_SPEC AssistanceMovementGenerator
             PointMovementGenerator<Creature>(0, _x, _y, _z, true) {}
 
         MovementGeneratorType GetMovementGeneratorType() const override { return ASSISTANCE_MOTION_TYPE; }
+	void Initialize(Unit&) override;
         void Finalize(Unit&) override;
 };
 
