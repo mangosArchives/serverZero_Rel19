e4eb607b54c8e633b5860f55ba97ea50d5637b51
Schmoozerd <schmoozerd@scriptdev2.com>
2013-04-13 00:00:33 +0300

    [z2363] Add support for condition_id to spell_area table
    Add a new column `condition_id` to table `spell_area`
    This column is intended to replace the columns
    - quest_start
    - quest_start_active
    - quest_end
    - racemask
    - gender
    
    Attention DB-Devs: A tool for convertion will be linked in the comments :)
    Final removal of these no more required columns will be done after some time, so that you can adapt and provide UPDATE support for this change :)
    
    Also simplify and gernalize the apply/remove code for these spell-area auras
    SQL-Changes for spell_area.condition_id
    
    Add first step of spell_area.conditionId
    
    Add second step of spell_area.conditionId
    
    This removes caching by quest_id
    This will be slightly slower, but only expected in case of quest-accept/remove cases.
    
    Some improvements and generalisations
    
    (based on commit [12440] - ef5e75a)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s1960] - 94905d1)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -6768,7 +6768,8 @@ char const* conditionSourceToStr[] =
     "gossip menu option",
     "event AI",
     "hardcoded",
-    "vendor's item check"
+    "vendor's item check",
+    "spell_area check"
 };
 
 // Checks if player meets the condition
