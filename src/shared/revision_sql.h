0066d473f3097c16b5e043a8671f9ba14df0dc36
billy1arm <antz@cix.co.uk>
2013-06-28 11:45:31 +0100

    [m2424] sync'd realmd version with other cores

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z1794_s1350_11716_09_characters_auction"
  #define REVISION_DB_MANGOS "required_r2420_01_mangos_spell_linked"
- #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
+ #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
 #endif // __REVISION_SQL_H__
