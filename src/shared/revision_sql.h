184ec1ad558076469446faab4782cff20e0d8c4f
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-08-18 15:37:43 +0200

    [m][2434] Renamed table scripted_event_id to scripted_event.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z1794_s1350_11716_09_characters_auction"
- #define REVISION_DB_MANGOS "required_r2420_01_mangos_spell_linked"
+ #define REVISION_DB_MANGOS "required_r2434_01_mangos_scripted_event"
  #define REVISION_DB_REALMD "required_z2426_01_realmd_relations"
 #endif // __REVISION_SQL_H__
