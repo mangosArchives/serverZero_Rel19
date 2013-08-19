b07f6355bafa2f851b34d8d9822e55ccef0ac70d
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-08-19 14:35:31 +0200

    - Removed unwanted database name in update query.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- sql/updates/z2434_sxxxx_xxxxx_01_mangos_scripted_event.sql
+++ sql/updates/z2434_sxxxx_xxxxx_01_mangos_scripted_event.sql
@@ -1,3 +1,3 @@
 ALTER TABLE  `db_version` CHANGE `required_r2420_01_mangos_spell_linked` `required_r2434_01_mangos_scripted_event` BIT(1) NULL DEFAULT NULL;
 
-RENAME TABLE  `zp_world_full`.`scripted_event_id` TO  `zp_world_full`.`scripted_event` ;
+RENAME TABLE  `scripted_event_id` TO  `scripted_event` ;
