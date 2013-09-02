83ccaa2a632e0a6abe9ea0c3ef1919fc5d453e00
hallabro <robin@hallabro.nu>
2013-09-02 20:54:31 +0200

    [c2484] Fix PostgreSQL bindings and add support to build directly with PostgreSQL instead of MySQL. (z2445)

--- src/shared/Database/DatabasePostgre.cpp
+++ src/shared/Database/DatabasePostgre.cpp
@@ -53,7 +53,7 @@ DatabasePostgre::~DatabasePostgre()
 
 SqlConnection* DatabasePostgre::CreateConnection()
 {
-    return new PostgreSQLConnection();
+    return new PostgreSQLConnection(*this);
 }
 
 PostgreSQLConnection::~PostgreSQLConnection()
