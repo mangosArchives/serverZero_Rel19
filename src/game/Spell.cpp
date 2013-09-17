49f3ba27204e3621792702fb69a07da20446d878
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-09-17 16:27:37 +0200

    When consuming potions not usable by their class, characters will now receive a matching error message.
    E.g. if a Warrior tries to consume a mana potion, an error will be sent.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5456,6 +5456,34 @@ SpellCastResult Spell::CheckItems()
                     }
 
                     Powers power = Powers(m_spellInfo->EffectMiscValue[i]);
+                    uint8 targetClass = m_targets.getUnitTarget()->getClass();
+                    /* Mana */
+                    if(power == POWER_MANA)
+                    {
+                        if (targetClass == CLASS_WARRIOR || targetClass == CLASS_ROGUE)
+                        {
+                            failReason = SPELL_FAILED_BAD_TARGETS;
+                            continue;
+                        }
+                    }
+                    /* Rage */
+                    else if (power == POWER_RAGE)
+                    {
+                        if(targetClass != CLASS_WARRIOR && targetClass != CLASS_DRUID)
+                        {
+                            failReason = SPELL_FAILED_BAD_TARGETS;
+                            continue;
+                        }
+                    }
+                    /* Energy */
+                    else if (power == POWER_ENERGY)
+                    {
+                        if(targetClass != CLASS_ROGUE && targetClass != CLASS_DRUID)
+                        {
+                            failReason = SPELL_FAILED_BAD_TARGETS;
+                            continue;
+                        }
+                    }
                     if (m_targets.getUnitTarget()->GetPower(power) == m_targets.getUnitTarget()->GetMaxPower(power))
                     {
                         failReason = SPELL_FAILED_ALREADY_AT_FULL_MANA;
