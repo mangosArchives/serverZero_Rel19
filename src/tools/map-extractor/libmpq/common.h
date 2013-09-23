6a1d5e039c14752b5103d6870450871be1b35b6c
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-09-23 21:29:26 +0200

    All code now uses a unified header, properly citing the GPL and crediting the rights of Blizzard Entertainment for World of Warcraft.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/tools/map-extractor/libmpq/common.h
+++ src/tools/map-extractor/libmpq/common.h
@@ -1,23 +1,25 @@
-/*
- *  common.h -- defines and structs used by the config files.
+/**
+ * mangos-zero is a full featured server for World of Warcraft in its vanilla
+ * version, supporting clients for patch 1.12.x.
  *
- *  Copyright (C) 2003 Maik Broemme <mbroemme@plusserver.de>
+ * Copyright (C) 2005-2013  MaNGOS project <http://getmangos.com>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  $Id: common.h,v 1.4 2004/02/12 00:41:55 mbroemme Exp $
+ * World of Warcraft, and all World of Warcraft or Warcraft art, images,
+ * and lore are copyrighted by Blizzard Entertainment, Inc.
  */
 
 #define LIBMPQ_CONF_FL_INCREMENT    512         /* i hope we did not need more :) */
