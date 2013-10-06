6127ab1941ef569dc9adb4cd83953cb8f0aa5d07
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-10-06 16:31:08 +0200

    Fixed a few compiler warnings.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -7098,7 +7098,7 @@ void Player::SendLoot(ObjectGuid guid, LootType loot_type)
 
             // not check distance for GO in case owned GO (fishing bobber case, for example)
             // And permit out of range GO with no owner in case fishing hole
-            if (!go || (loot_type != LOOT_FISHINGHOLE && (loot_type != LOOT_FISHING && loot_type != LOOT_FISHING_FAIL || go->GetOwnerGuid() != GetObjectGuid()) && !go->IsWithinDistInMap(this, INTERACTION_DISTANCE)))
+            if (!go || (loot_type != LOOT_FISHINGHOLE && ((loot_type != LOOT_FISHING && loot_type != LOOT_FISHING_FAIL) || go->GetOwnerGuid() != GetObjectGuid()) && !go->IsWithinDistInMap(this, INTERACTION_DISTANCE)))
             {
                 SendLootRelease(guid);
                 return;
@@ -15093,7 +15093,7 @@ DungeonPersistentState* Player::GetBoundInstanceSaveForSelfOrGroup(uint32 mapid)
     {
         InstanceGroupBind* groupBind = NULL;
         if (Group* group = GetGroup())
-            if (groupBind = group->GetBoundInstance(mapid))
+            if ((groupBind = group->GetBoundInstance(mapid)))
                 { state = groupBind->state; }
     }
 
