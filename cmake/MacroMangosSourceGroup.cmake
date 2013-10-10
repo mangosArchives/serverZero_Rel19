6b74bd8083faf0f50c0baf21cbb7e85143fa86bf
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-10-11 00:13:24 +0200

    Prepare source grouping for Win32 builds.
    Source and header files can be grouped for Visual Studio solutions.
    Currently these are just split into headers and sources. This macro
    allows us to create child groups below sources and headers based on
    a given topic.
    
    An example would be:
    
        mangos_source_group_topic(${target_sources} PacketHandlers)
    
    This would add headers and sources below into the folders
    
    * Header Files\PacketHandlers
    * Source Files\PacketHandlers
    
    The intent is to provide a more readable project for larger sources.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- cmake/MacroMangosSourceGroup.cmake
+++ cmake/MacroMangosSourceGroup.cmake
@@ -10,11 +10,27 @@ macro(mangos_source_group
            (${_SRC_EXT} MATCHES ".hpp") OR
            (${_SRC_EXT} MATCHES ".hh"))
             source_group("Header Files" FILES ${_SRC})
-        elseif((${_SRC_EXT} MATCHES ".rc") OR
-            (${_SRC_EXT} MATCHES ".ico"))
-            source_group("Resource Files" FILES ${_SRC})
         else()
             source_group("Source Files" FILES ${_SRC})
+        endif()
+    endforeach()
+
+    unset(_SRC)
+    unset(_SRC_EXT)
+endmacro()
+
+macro(mangos_source_group_topic
+    sources
+    topic)
+
+    foreach(_SRC ${sources})
+        get_filename_component(_SRC_EXT ${_SRC} EXT)
+        if((${_SRC_EXT} MATCHES ".h") OR
+           (${_SRC_EXT} MATCHES ".hpp") OR
+           (${_SRC_EXT} MATCHES ".hh"))
+            source_group("Header Files\\${topic}" FILES ${_SRC})
+        else()
+            source_group("Source Files\\${topic}" FILES ${_SRC})
         endif()
     endforeach()
 
