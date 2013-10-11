1a69bdbf94318bae72e8be28b43ee3af373c9f28
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-10-11 15:22:01 +0200

    Prevent resource leaks in DBC loader.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/shared/Database/DBCFileLoader.cpp
+++ src/shared/Database/DBCFileLoader.cpp
@@ -43,29 +43,47 @@ bool DBCFileLoader::Load(const char* filename, const char* fmt)
     if (!f) { return false; }
 
     if (fread(&header, 4, 1, f) != 1)                       // Number of records
-        { return false; }
+    {
+        fclose(f);
+        return false;
+    }
 
     EndianConvert(header);
-    if (header != 0x43424457)
-        { return false; }                                       //'WDBC'
+    if (header != 0x43424457)                               //'WDBC'
+    {
+        fclose(f);
+        return false;
+    }
 
     if (fread(&recordCount, 4, 1, f) != 1)                  // Number of records
-        { return false; }
+    {
+        fclose(f);
+        return false;
+    }
 
     EndianConvert(recordCount);
 
     if (fread(&fieldCount, 4, 1, f) != 1)                   // Number of fields
-        { return false; }
+    {
+        fclose(f);
+        return false;
+    }
 
     EndianConvert(fieldCount);
 
     if (fread(&recordSize, 4, 1, f) != 1)                   // Size of a record
-        { return false; }
+    {
+        fclose(f);
+        return false;
+    }
 
     EndianConvert(recordSize);
 
     if (fread(&stringSize, 4, 1, f) != 1)                   // String size
-        { return false; }
+    {
+        fclose(f);
+        return false;
+    }
 
     EndianConvert(stringSize);
 
@@ -84,7 +102,10 @@ bool DBCFileLoader::Load(const char* filename, const char* fmt)
     stringTable = data + recordSize * recordCount;
 
     if (fread(data, recordSize * recordCount + stringSize, 1, f) != 1)
-        { return false; }
+    {
+        fclose(f);
+        return false;
+    }
 
     fclose(f);
     return true;
