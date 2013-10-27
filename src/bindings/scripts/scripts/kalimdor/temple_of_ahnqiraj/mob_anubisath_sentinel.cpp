85583417d45e74166689b6617ded93dda4eefd15
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-10-27 11:04:07 +0100

    Fixed a compiler warning.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/bindings/scripts/scripts/kalimdor/temple_of_ahnqiraj/mob_anubisath_sentinel.cpp
+++ src/bindings/scripts/scripts/kalimdor/temple_of_ahnqiraj/mob_anubisath_sentinel.cpp
@@ -83,7 +83,7 @@ struct MANGOS_DLL_DECL npc_anubisath_sentinelAI : public ScriptedAI
             reader.PSendSysMessage("Anubisath Sentinel - proper group found");
         }
         else
-            { reader.PSendSysMessage("Anubisath Sentinel - not correct number of mobs for group found. Number found %u, should be %u", m_lAssistList.size(), MAX_BUDDY); }
+            { reader.PSendSysMessage("Anubisath Sentinel - not correct number of mobs for group found. Number found %lu, should be %u", m_lAssistList.size(), MAX_BUDDY); }
     }
 
     void JustReachedHome() override
