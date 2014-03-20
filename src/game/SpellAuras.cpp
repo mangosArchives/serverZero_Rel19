5f20b567ba298110b51300fe5d9f0b880170d88f
Patman64 <patman256@yahoo.com>
2014-03-20 08:40:59 +0100

    [c2478] Don't do periodic heal ticks if the target is already at max health (z2439)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4452,6 +4452,10 @@ void Aura::PeriodicTick()
             if (!pCaster)
                 { return; }
 
+            // Don't heal target if it is already at max health
+            if (target->GetHealth() == target->GetMaxHealth())
+                return;
+
             // heal for caster damage (must be alive)
             if (target != pCaster && spellProto->SpellVisual == 163 && !pCaster->IsAlive())
                 { return; }
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -24,5 +24,5 @@
 
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
-#define REVISION_NR "2477"
+#define REVISION_NR "2478"
 #endif // __REVISION_NR_H__
