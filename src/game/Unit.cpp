c48037d89dbff5a8a100afb33ee4a748875f4b43
Cyberium <cybermomo@techemail.com>
2014-03-29 16:27:20 +0100

    [c2487] Simple fix to avoid corpse moving in some situation. (z2449)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6968,7 +6968,7 @@ void Unit::SetDeathState(DeathState s)
         RemoveGuardians();
         UnsummonAllTotems();
 
-        StopMoving();
+        StopMoving(true);
         i_motionMaster.Clear(false, true);
         i_motionMaster.MoveIdle();
 
