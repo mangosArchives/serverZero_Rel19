e8fca3e523acef84c00e009445fb2e51f0cc6a1b
Antz <billy1arm@users.noreply.github.com>
2014-05-29 15:38:08 +0100

    [Scripts] Release 0.19 prep

--- src/bindings/scripts/scripts/kalimdor/darkshore.cpp
+++ src/bindings/scripts/scripts/kalimdor/darkshore.cpp
@@ -714,7 +714,7 @@ struct MANGOS_DLL_DECL npc_rabid_bearAI : public ScriptedAI
     void MoveInLineOfSight(Unit* pWho) override
     {
         if (!m_uiCheckTimer && pWho->GetTypeId() == TYPEID_UNIT && pWho->GetEntry() == NPC_THARNARIUN_TREETENDER &&
-        pWho->IsWithinDist(m_creature, 2 * INTERACTION_DISTANCE, false))
+                pWho->IsWithinDist(m_creature, 2 * INTERACTION_DISTANCE, false))
         {
             // Possible related spell: 9455 9372
             m_creature->ForcedDespawn(1000);
@@ -748,7 +748,7 @@ struct MANGOS_DLL_DECL npc_rabid_bearAI : public ScriptedAI
                     // get player
                     Unit* pTrapOwner = pTrap->GetOwner();
                     if (pTrapOwner && pTrapOwner->GetTypeId() == TYPEID_PLAYER &&
-                    ((Player*)pTrapOwner)->GetQuestStatus(QUEST_PLAGUED_LANDS) == QUEST_STATUS_INCOMPLETE)
+                            ((Player*)pTrapOwner)->GetQuestStatus(QUEST_PLAGUED_LANDS) == QUEST_STATUS_INCOMPLETE)
                     {
                         ((Player*)pTrapOwner)->KilledMonsterCredit(m_creature->GetEntry(), m_creature->GetObjectGuid());
                         m_creature->GetMotionMaster()->MoveFollow(pTrapOwner, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
--- src/bindings/scripts/sd2_revision_nr.h
+++ src/bindings/scripts/sd2_revision_nr.h
@@ -25,5 +25,5 @@
 
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
-  #define SD2_REVISION_NR "z2687"
+#define SD2_REVISION_NR "Rel0.19"
 #endif // __SD2_REVISION_NR_H__
