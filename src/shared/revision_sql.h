5d107a99c5064fdb4772c2dde136740dccb067bb
Neotmiren <>
2014-05-30 11:32:36 +0100

    [DB] Makes Paladin's talent Benediction reduces mana cost of Seal of Crusader and Seal of Command

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -25,6 +25,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
 #define REVISION_DB_CHARACTERS "required_z1794_s1350_11716_09_characters_auction"
-#define REVISION_DB_MANGOS "required_z2498_mangos_creature_template_classlevelstats"
+ #define REVISION_DB_MANGOS "required_z2514_xxxxx_01_mangos_spell_affect"
 #define REVISION_DB_REALMD "required_z2426_01_realmd_relations"
 #endif // __REVISION_SQL_H__
