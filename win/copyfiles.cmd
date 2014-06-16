e5fbadc479fe8433ac83c53fe9362311182ef097
Antz <billy1arm@users.noreply.github.com>
2014-06-16 22:06:14 +0100

    [Scripts] Moved the script library out of bindings and fixed up references

--- win/copyfiles.cmd
+++ win/copyfiles.cmd
@@ -1,30 +1,38 @@
 echo ******************************************************
 echo ** COPY STEP                                        **
 echo ******************************************************
+
 echo .
 echo **********************
 echo ** Source Path      **
 echo **********************
 echo Source: %1
+
 echo .
 echo **********************
 echo ** Destination Path **
 echo **********************
 echo Destination: %2
+
 echo .
 echo **********************
 echo ** Current Path     **
 echo **********************
 cd
+
 echo .
 echo **********************
 echo ** DLL's            **
 echo **********************
 copy ..\%1\*.dll %2
+
+echo .
 echo **********************
 echo ** mangosd.conf     **
 echo **********************
 copy ..\..\src\mangosd\mangosd.conf.dist.in %2
+
+echo .
 echo **********************
 echo ** ahbot.conf       **
 echo **********************
@@ -36,4 +44,21 @@ echo ** Extraction Tools **
 echo **********************
 copy ..\..\src\tools\Extractor_Binaries\*.* %2
 
+echo .
+echo **********************
+echo **  OpenSSL DLL's   **
+echo **********************
+copy ..\..\dep\lib\libeay32.dll %2
+copy ..\..\dep\lib\ssleay32.dll %2
+copy ..\..\dep\lib\\libssl32.dll %2
 
+echo .
+echo **********************
+echo **   MySQL DLL's    **
+echo **********************
+copy ..\..\dep\lib\libmysql.dll %2
+
+echo .
+echo **********************
+echo * Copy Step Complete *
+echo **********************
