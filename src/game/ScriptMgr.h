b2edf1b470e78d4cb722a99f7f598ecc00c13845
Xfurry <xfurry@scriptdev2.com>
2014-08-04 22:50:37 +0200

    [Core] Implement SCRIPT_COMMAND_SEND_AI_EVENT_AROUND for dbscripts engine [z2497]
    This allows more flexibility in interaction between eventAI and dbscripts

--- src/game/ScriptMgr.h
+++ src/game/ScriptMgr.h
@@ -106,7 +106,8 @@ enum ScriptCommand                                          // resSource, resTar
     SCRIPT_COMMAND_RESERVED_1               = 33,           // reserved for 3.x and later
     SCRIPT_COMMAND_TERMINATE_COND           = 34,           // datalong = condition_id, datalong2 = if != 0 then quest_id of quest that will be failed for player's group if the script is terminated
     // data_flags & SCRIPT_FLAG_COMMAND_ADDITIONAL terminate when condition is false ELSE terminate when condition is true
-    SCRIPT_COMMAND_TURN_TO                  = 35,           // resSource = Unit, resTarget = Unit/none
+    SCRIPT_COMMAND_SEND_AI_EVENT_AROUND     = 35,           // resSource = Creature, resTarget = Unit, datalong = AIEventType, datalong2 = radius
+    SCRIPT_COMMAND_TURN_TO                  = 36,           // resSource = Unit, resTarget = Unit/none
 };
 
 #define MAX_TEXT_ID 4                                       // used for SCRIPT_COMMAND_TALK
@@ -324,12 +325,18 @@ struct ScriptInfo
             uint32 failQuest;                               // datalong2
         } terminateCond;
 
+        struct                                              // SCRIPT_COMMAND_SEND_AI_EVENT_AROUND (35)
+        {
+            uint32 eventType;                               // datalong
+            uint32 radius;                                  // datalong2
+        } sendAIEvent;
+
         struct
         {
             uint32 data[2];
         } raw;
 
-        struct                                              // SCRIPT_COMMAND_TURN_TO (35)
+        struct                                              // SCRIPT_COMMAND_TURN_TO (36)
         {
             uint32 targetId;                                // datalong
             uint32 empty1;                                  // datalong2
