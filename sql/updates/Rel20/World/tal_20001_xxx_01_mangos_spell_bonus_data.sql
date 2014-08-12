f7035be6ca1ca4f81cc36b913dd7f03e58c2fe22
Talendrys <cedric_lesi@hotmail.com>
2014-08-12 21:56:31 +0200

    Revert "Adding support of one-hand/two-hand custom spell bonus coeff in the core."
    This reverts commit e72f29960d543b0d082fc9f0b6ec6b4c4fa1f138.

--- sql/updates/Rel20/World/tal_20001_xxx_01_mangos_spell_bonus_data.sql
+++
@@ -1,4 +0,0 @@
--- Update table structure for one-hand/two-hand custom spell coeff.
-ALTER TABLE `mangos`.`spell_bonus_data`
-ADD COLUMN `one_hand_direct_bonus` FLOAT NOT NULL DEFAULT 0 AFTER `direct_bonus`,
-ADD COLUMN `two_hand_direct_bonus` FLOAT NOT NULL DEFAULT 0 AFTER `one_hand_direct_bonus`;
