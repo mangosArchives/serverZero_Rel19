961fd14582cbaf1719af75aeea21fbc65b5e5bc6
Antz <billy1arm@users.noreply.github.com>
2014-08-15 21:53:30 +0100

    [DB] Remove dbname from update queries

--- sql/updates/Rel20/World/20003_01_mangos_spell_bonus_data.sql
+++ sql/updates/Rel20/World/20003_01_mangos_spell_bonus_data.sql
@@ -2,7 +2,7 @@ ALTER TABLE db_version CHANGE COLUMN required_20002_creature_template_power requ
 
 -- Update table structure for one-hand/two-hand custom spell coeff.
 -- Add also support of custom coeff for done/taken damage.
-ALTER TABLE `mangos`.`spell_bonus_data`
+ALTER TABLE `spell_bonus_data`
 ADD COLUMN `one_hand_direct_bonus` FLOAT NOT NULL DEFAULT 0 AFTER `direct_bonus`,
 ADD COLUMN `two_hand_direct_bonus` FLOAT NOT NULL DEFAULT 0 AFTER `one_hand_direct_bonus`,
 ADD COLUMN `direct_bonus_done` FLOAT NOT NULL DEFAULT 0 AFTER `two_hand_direct_bonus`,
--- sql/updates/Rel20/World/20003_03_mangos_spell_bonus_data.sql
+++ sql/updates/Rel20/World/20003_03_mangos_spell_bonus_data.sql
@@ -3,5 +3,5 @@ ALTER TABLE db_version CHANGE COLUMN required_20003_02_mangos_spell_bonus_data r
 -- Seal of Command fix
 -- 20% SpellDamageBonusDone
 -- 29% SpellDamageBonusTaken
-UPDATE `mangos`.`spell_bonus_data` SET `direct_bonus`='0', `direct_bonus_done`='0.2', `direct_bonus_taken`='0.29' WHERE `entry`='20424';
+UPDATE `spell_bonus_data` SET `direct_bonus`='0', `direct_bonus_done`='0.2', `direct_bonus_taken`='0.29' WHERE `entry`='20424';
 
