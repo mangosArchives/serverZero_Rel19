c68d0f8bab614fd88fdad34a7ada522e36405cc8
Antz <billy1arm@users.noreply.github.com>
2014-08-15 21:26:20 +0100

    [Core] Update Default char Db for Rel20

--- sql/characters.sql
+++ sql/characters.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `character_db_version`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `character_db_version` (
-  `required_19002_02_character_whispers` bit(1) DEFAULT NULL
+  `required_20000_Release_019_Initial` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
