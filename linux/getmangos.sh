6a67ce494da1c2492973fe0b83e1d3be7c76df48
The-Great-Sephiroth <ryan.b.ashley@gmail.com>
2014-10-29 10:34:51 -0400

    Added GNU license to Linux build script

--- linux/getmangos.sh
+++ linux/getmangos.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-############################################################
-# MaNGOS Build Automation Script                           #
-# Written By: Ryan Ashley                                  #
-############################################################
+###############################################################################
+# MaNGOS Build Automation Script                                              #
+# Written By: Ryan Ashley                                                     #
+# Copyright (c) 2014 MaNGOS Project                                           #
+# http://getmangos.eu/                                                        #
+#                                                                             #
+# This program is free software; you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program; if not, write to the Free Software                 #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA     #
+###############################################################################
 
 # Global variables
 DLGAPP="whiptail"
