4a4b6b481104dfdadcc022d1a5a78a288e804234
Ryan Ashley <The-Great-Sephiroth@users.noreply.github.com>
2014-12-24 09:31:16 -0500

    Fix for memory spikes on pet despawn

--- src/game/Object/Pet.cpp
+++ src/game/Object/Pet.cpp
@@ -550,7 +550,7 @@ void Pet::Update(uint32 update_diff, uint32 diff)
         case CORPSE:
         {
             Unsummon(PET_SAVE_NOT_IN_SLOT);
-            return;
+            break;
         }
         case ALIVE:
         {
