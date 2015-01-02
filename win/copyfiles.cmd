0493e049b8916a5d268030c3ccfba1232172e3f9
Antz <Antz@getMaNGOS.co.uk>
2015-01-02 22:07:43 +0000

    [Build] Fix build on Win with spaces in pathname

--- win/copyfiles.cmd
+++ win/copyfiles.cmd
@@ -8,7 +8,7 @@ if not exist ..\..\build goto PathError1:
 :path2
 if not exist ..\..\build\bin goto PathError2:
 :path3
-if not exist ..\..\build\bin\%3 goto PathError3:
+if not exist "..\..\build\bin\%3" goto PathError3:
 goto pathsok:
 
 :PathError1:
@@ -23,15 +23,15 @@ goto path3:
 
 :PathError3:
 echo Creating: build\bin\%3 Folder
-mkdir ..\..\build\bin\%3
+mkdir "..\..\build\bin\%3"
 goto pathsok:
 
 :copypdb
 echo.
 echo = Copy Debug PDB's =
 echo ====================
-echo copy %1\%3\*.pdb %2\%3
-copy %1\%3\*.pdb %2\%3
+echo copy "%1\%3\*.pdb" "%2\%3"
+copy "%1\%3\*.pdb" "%2\%3"
 goto afterpdb
 
 
@@ -49,8 +49,11 @@ echo Path
 cd
 
 echo.
-echo = Copy Server Exe's =
-echo =====================
+echo = Copy Server Exe's / Dll's =
+echo =============================
+echo From: %1\%3
+echo   To: %2\%3 
+echo =============================
 copy %1\%3\ace.dll %2\%3
 copy %1\%3\mangosd.exe %2\%3
 copy %1\%3\realmd.exe %2\%3
@@ -58,6 +61,9 @@ copy %1\%3\realmd.exe %2\%3
 echo.
 echo = Copy Extraction Exe's =
 echo =========================
+echo From: %1\%3
+echo   To: %2\%3 
+echo =====================
 copy %1\%3\map-extractor.exe %2\%3
 copy %1\%3\movemap-generator.exe %2\%3
 copy %1\%3\vmap-assembler.exe %2\%3
@@ -70,38 +76,38 @@ if %3 == Debug goto copypdb:
 echo .
 echo =   mangosd.conf   =
 echo ====================
-copy ..\..\src\mangosd\mangosd.conf.dist.in %2\%3
+copy "..\..\src\mangosd\mangosd.conf.dist.in" "%2\%3"
 
 echo .
 echo =   realmd.conf   =
 echo ====================
-copy ..\..\src\realmd\realmd.conf.dist.in %2\%3
+copy "..\..\src\realmd\realmd.conf.dist.in" "%2\%3"
 
 echo .
 echo =    ahbot.conf    =
 echo ====================
-copy ..\..\src\game\AuctionHouseBot\ahbot.conf.dist.in %2\%3
+copy "..\..\src\game\AuctionHouseBot\ahbot.conf.dist.in" "%2\%3"
 
 echo .
 echo = Extraction Scripts =
 echo ======================
-copy ..\..\src\tools\Extractor_Binaries\*.sh %2\%3
-copy ..\..\src\tools\Extractor_Binaries\*.txt %2\%3
-copy ..\..\src\tools\Extractor_Binaries\*.bat %2\%3
+copy "..\..\src\tools\Extractor_Binaries\*.sh" "%2\%3"
+copy "..\..\src\tools\Extractor_Binaries\*.txt" "%2\%3"
+copy "..\..\src\tools\Extractor_Binaries\*.bat" "%2\%3"
 pause
 
 echo .
 echo **********************
 echo **  OpenSSL DLL's   **
 echo **********************
-copy ..\..\dep\lib\%4\libeay32.dll %2\%3
-copy ..\..\dep\lib\%4\ssleay32.dll %2\%3
+copy "..\..\dep\lib\%4\libeay32.dll" "%2\%3"
+copy "..\..\dep\lib\%4\ssleay32.dll" "%2\%3"
 
 echo .
 echo **********************
 echo **   MySQL DLL's    **
 echo **********************
-copy ..\..\dep\lib\%4\libmysql.dll %2\%3
+copy "..\..\dep\lib\%4\libmysql.dll" "%2\%3"
 pause
 
 echo .
