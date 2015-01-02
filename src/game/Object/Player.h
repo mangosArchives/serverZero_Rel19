a6212a7798cec71fbdb4848dcea0f06eda95c362
Cyberium <cybermomo@techemail.com>
2015-01-02 09:53:53 +0000

    [Core] Rewrite rest management (c2568) Fix Rest when logged in not working in some case. Also fix rest bonus according to wowwiki.
    This need to be backported in all core.
    Thank to @TheTrueAnimal for pointing.

--- src/game/Object/Player.h
+++ src/game/Object/Player.h
@@ -1038,6 +1038,15 @@ class MANGOS_DLL_SPEC Player : public Unit
         }
         void SetRestBonus(float rest_bonus_new);
 
+        /**
+        * \brief: compute rest bonus
+        * \param: time_t timePassed > time from last check
+        * \param: bool offline      > is the player was offline?
+        * \param: bool inRestPlace  > if it was offline, is the player was in city/tavern/inn?
+        * \returns: float
+        **/
+        float ComputeRest(time_t timePassed, bool offline = false, bool inRestPlace = false);
+
         RestType GetRestType() const
         {
             return rest_type;
